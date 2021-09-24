95314742987f8a33bd3a8518fa790535c3635146
Robert <rcreech93@gmail.com>
2021-09-24 10:31:48 -0400

    Added template

---
+++ conversion.cpp
@@ -0,0 +1,37 @@
+//Convert temp
+//Using formula Fahrenheit = Celsius * (212 -32)/100 + 32
+
+//
+
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main(int nNumberOfArgs, char* pszArgs[])
+{
+	//Enter temp in celsius
+	int celsius;
+	cout << "Enter the temperature in Celsius: ";
+	cin >> celsius;
+
+	//Calculate conversion factor for celsius
+
+	int factor;
+	factor = 212 - 32;
+
+	//Use conversion factor to convert celsius to fahrenheit
+
+	int fahrenheit;
+	fahrenheit = factor * celsius / 100 + 32;
+
+	//Output the results followed by a new line;
+
+	cout << "Fahrenheit value is: ";
+	cout << fahrenheit << endl;
+
+	//Wait for the user to see the results before the program terminates
+	system("PAUSE");
+	return 0;
+}
