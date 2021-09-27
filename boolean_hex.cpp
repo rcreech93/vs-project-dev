c84fe0b0498b926c6286e2bb4f02c15fae95a877
rcreech93 <57920890+rcreech93@users.noreply.github.com>
2021-09-27 11:25:41 -0400

    Finish boolean hex test

--- boolean_hex.cpp
+++ boolean_hex.cpp
@@ -19,6 +19,29 @@ int main(int nNumberOfArgs, char* pszArgs[])
 	//
     //Sets output to be in Hexadecimal
     cout.setf(cout.hex);
+    //Initialie two Arguments
+
+    int nArg1;
+    nArg1 = 0x1234;
+
+    int nArg2;
+    nArg2 = 0x00ff;
+
+    //Pefrom each operation check on both arguments
+
+    //First NOT operation
+
+    cout << "Arg1           = 0x" << nArg1 << "\n";
+    cout << "Arg2           = 0x" << nArg2 << "\n";
+    cout << "~nArg1         = 0x" << ~nArg1 << "\n";
+    cout << "~nArg2         = 0x" << ~nArg2 << "\n";
+
+    //Next binary operation
+
+    cout << "nArg1 & nArg2 = 0x" << (nArg1 & nArg2) << "\n";
+    cout << "nArg1 | nArg2 = 0x" << (nArg1 | nArg2) << "\n";
+    cout << "nArg1 ^ nArg2 = 0x" << (nArg1 ^ nArg2) << "\n";
+    
 
 
 	//Add system pause at the end so the user can see the output of the program
