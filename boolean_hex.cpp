de67b775e4dd451e6a51a3472fcd64fde0661da9
rcreech93 <57920890+rcreech93@users.noreply.github.com>
2021-09-27 11:14:30 -0400

    Boolean hex testing

---
+++ boolean_hex.cpp
@@ -0,0 +1,28 @@
+//C++ console application template
+//Robert Creech
+
+//Templates provide a baseline for the developer to use 
+//
+//
+//This template serves as a baseline for c++ console applications and applies basic header information
+//Including any functions that are typically used
+
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main(int nNumberOfArgs, char* pszArgs[])
+{
+	//C++ code goes here
+	//
+    //Sets output to be in Hexadecimal
+    cout.setf(cout.hex);
+
+
+	//Add system pause at the end so the user can see the output of the program
+
+	system("PAUSE");
+	return 0;
+}
